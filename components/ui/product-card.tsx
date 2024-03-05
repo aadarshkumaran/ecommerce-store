1929495cd75e75c46da4f53363fd56d7e60391b8
Aadarsh Kumaran <aadarshkumaranak@gmail.com>
2024-03-05 16:30:53 +0530

    Product Preview Modal Components (Completed), Add To Cart (In Progress)

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -7,17 +7,25 @@ import IconButton from "@/components/ui/icon-button";
 import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import { usePreviewModal } from "@/hooks/use-preview-modal";
 
-interface ProductCard{
-    data : Product;
+interface ProductCard {
+    data: Product;
 }
 
-const ProductCard: React.FC<ProductCard> = ({data}) => {
-    const router = useRouter()
+const ProductCard: React.FC<ProductCard> = ({ data }) => {
+    const router = useRouter();
+    const previewModal = usePreviewModal()
 
-    const handleClick = () =>{
+    const handleClick = () => {
         router.push(`/product/${data?.id}`)
-    }
+    };
+
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+        previewModal.onOpen(data);
+    };
 
 
     return (
@@ -25,18 +33,21 @@ const ProductCard: React.FC<ProductCard> = ({data}) => {
             {/* Images and Actions */}
             <div className="aspect-square rounded-xl bg-gray-100 relative">
                 <Image src={data?.images?.[0]?.url}
-                fill
-                alt="Image"
-                className="aspect-square object-cover rounded-md"
+                    fill
+                    alt="Image"
+                    className="aspect-square object-cover rounded-md"
                 />
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
-                        <IconButton
-                        onClick={()=>{}} icon={<Expand size={20} className="text-gray-600"/>}
-                        />
-                        <IconButton
-                        onClick={()=>{}} icon={<ShoppingCart size={20} className="text-gray-600"/>}
-                        />
+                        <div onClick={onPreview}>
+                            <IconButton icon={<Expand size={20} className="text-gray-600" />}
+                            />
+                        </div>
+                        <div onClick={() => { }}>
+                            <IconButton
+                                icon={<ShoppingCart size={20} className="text-gray-600" />}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -46,7 +57,7 @@ const ProductCard: React.FC<ProductCard> = ({data}) => {
                 <p className="text-sm text-gray-500">{data.category?.name}</p>
             </div>
             {/* Price */}
-            <Currency value={data?.price}/>
+            <Currency value={data?.price} />
         </div>
     );
 }
