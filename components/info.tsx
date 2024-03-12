8ff7a22da8fada9df9ea95b287e8f5698b874c8a
Aadarsh Kumaran <aadarshkumaranak@gmail.com>
2024-03-12 14:52:56 +0530

    Add to Cart Button Functionality (Completed)

--- components/info.tsx
+++ components/info.tsx
@@ -5,6 +5,7 @@ import { ShoppingCart } from "lucide-react";
 import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
+import { useCart } from "@/hooks/use-cart";
 
 interface InfoProps {
     data: Product
@@ -13,6 +14,10 @@ interface InfoProps {
 const Info: React.FC<InfoProps> = ({
     data
 }) => {
+    const cart = useCart();
+    const onAddToCart = () =>{
+        cart.addItem(data)
+    }
     return (<>
         <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
         <div className="mt-3 flex items-end justify-between">
@@ -33,7 +38,7 @@ const Info: React.FC<InfoProps> = ({
             </div>
         </div>
         <div className="mt-10 flex items-center gap-x-3">
-            <Button className="flex items-center gap-x-2">
+            <Button onClick={onAddToCart} className="flex items-center gap-x-2">
                 Add To Cart
                 <ShoppingCart/>
             </Button>
